17969e8e084c3c78834fb8e6a249611ace93c351
adityash97 <adityash212@gmail.com>
2022-12-15 01:15:15 +0530

    api with drf

--- watchmate/watchlist_app/views.py
+++ watchmate/watchlist_app/views.py
@@ -1,15 +1,15 @@
-from django.shortcuts import render
-from .models import Movie
-from django.http import JsonResponse
-# Create your views here.
+# from django.shortcuts import render
+# from .models import Movie
+# from django.http import JsonResponse
+# # Create your views here.
 
-def movie_list(request):
-    movies = Movie.objects.all()
-    data = {'movies': list(movies.values())}
-    return JsonResponse(data)
+# def movie_list(request):
+#     movies = Movie.objects.all()
+#     data = {'movies': list(movies.values())}
+#     return JsonResponse(data)
 
-def movie_details(request,pk):
-    movie = Movie.objects.get(pk=pk)
-    data = {'name':movie.name,'description':movie.description,'actie':movie.active}
+# def movie_details(request,pk):
+#     movie = Movie.objects.get(pk=pk)
+#     data = {'name':movie.name,'description':movie.description,'actie':movie.active}
     
-    return JsonResponse(data)
+#     return JsonResponse(data)
