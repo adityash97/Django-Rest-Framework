73580a2eecff3260f77752242c9ee2058fa4ea96
adityash97 <adityash212@gmail.com>
2023-01-07 16:33:12 +0530

    mixins & Concrete View Classes

--- MovieList/movielist_app/api/urls.py
+++ MovieList/movielist_app/api/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from .views import movieList,movieDetails,ReviewListAPIView,ReviewDetailsAPIView
+from .views import MovieListAPIView,MovieDetailsAPIView,ReviewListAPIView,ReviewDetailsAPIView
 
 urlpatterns =[
-    path('movie/',movieList,name='movie-list'),
-    path('movie/<int:pk>/',movieDetails,name='movie-details'),
+    path('movie/',MovieListAPIView.as_view(),name='movie-list'),
+    path('movie/<int:pk>/',MovieDetailsAPIView.as_view(),name='movie-details'),
     path('review/',ReviewListAPIView.as_view(),name='review-list'),
     path('review/<int:pk>/',ReviewDetailsAPIView.as_view(),name='review-details'),
     
--- MovieList/movielist_app/api/views.py
+++ MovieList/movielist_app/api/views.py
@@ -2,47 +2,33 @@
 from rest_framework.views import APIView
 from rest_framework import status 
 from rest_framework.response import Response
-from rest_framework.decorators import api_view
 from rest_framework.views import APIView
+from rest_framework import generics,mixins
 from movielist_app.models import Movie,Review
 from .serializers import MovieSerializer,ReviewSerializer
 
-@api_view(['GET','POST'])
-def movieList(request):
-    if request.method == 'GET':
-        queryset = Movie.objects.all()
-        serializer = MovieSerializer(queryset,many=True)
-        return Response(serializer.data,status=status.HTTP_200_OK)
-    elif request.method == 'POST':
-        serializer = MovieSerializer(data = request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors)
+class MovieListAPIView(generics.ListCreateAPIView):
+    queryset = Movie.objects.all()
+    serializer_class = MovieSerializer
 
-@api_view(['GET','PUT','DELETE'])
-def movieDetails(request,pk):
-    try:
-        queryset = Movie.objects.get(pk=pk)
-    except:
-        return Response({'msg':'DoesNotExist'},status=status.HTTP_204_NO_CONTENT)
-    if request.method == 'GET':
-        serializer = MovieSerializer(queryset)
-        return Response(serializer.data,status=status.HTTP_200_OK)
-    elif request.method == "PUT":
-        serializer = MovieSerializer(queryset,data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data,status=status.HTTP_200_OK)
-        else:
-            return Response(serializer.errors,status=status.HTTP_404_NOT_FOUND)
-    elif request.method == "DELETE":
-        try:
-            queryset.delete()
-            return Response({'msg':'Deleted successfully'},status=status.HTTP_204_NO_CONTENT)
-        except queryset.DoesNotExist:
-             return Response({'msg':'DoesNotExist'},status=status.HTTP_204_NO_CONTENT)
+
+class MovieDetailsAPIView(generics.GenericAPIView,mixins.UpdateModelMixin,
+                mixins.RetrieveModelMixin,mixins.DestroyModelMixin):
+    queryset = Movie.objects.all()
+    serializer_class = MovieSerializer
     
+    def get(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
+
+    def put(self, request, *args, **kwargs):
+        return self.update(request, *args, **kwargs)
+
+    def delete(self, request, *args, **kwargs):
+        return self.destroy(request, *args, **kwargs)
+
+
+
+  
 class ReviewListAPIView(APIView):
     def get(self,request):
         queryset = Review.objects.all()
@@ -57,42 +43,9 @@ class ReviewListAPIView(APIView):
         else:
             return Response(serializer.errors,status=status.HTTP_404_NOT_FOUND)
 
-class ReviewDetailsAPIView(APIView):
+class ReviewDetailsAPIView(generics.RetrieveUpdateDestroyAPIView):        
+    queryset = Review.objects.all()
+    serializer_class = ReviewSerializer
     
-    def get(self,request,pk):
-        try:
-            queryset = Review.objects.get(pk=pk)
-        except:
-            return Response({'msg':'DoesNotExist'},status=status.HTTP_204_NO_CONTENT)
-        serilaizer = ReviewSerializer(queryset)
-        return Response(serilaizer.data,status=status.HTTP_200_OK)
-        
-    
-    def put(self,request,pk):
-        try:
-            queryset = Review.objects.get(pk=pk)
-        except:
-            return Response({'msg':'DoesNotExist'},status=status.HTTP_204_NO_CONTENT)
-        serilaizer = ReviewSerializer(queryset,data=request.data)
-        if serilaizer.is_valid():
-            serilaizer.save()
-            return Response(serilaizer.data,status=status.HTTP_200_OK)
-        else:
-            return Response(serilaizer.errors,status=status.HTTP_400_BAD_REQUEST)
-            
-        
-    def delete(self,request,pk):
-        try:
-            queryset = Review.objects.get(pk=pk)
-        except:
-            return Response({'msg':'DoesNotExist'},status=status.HTTP_204_NO_CONTENT)
-        
-        try:
-            queryset.delete()
-            return Response({'msg':'Deleted successfully'},status=status.HTTP_204_NO_CONTENT)
-        except queryset.DoesNotExist:
-             return Response({'msg':'DoesNotExist'},status=status.HTTP_204_NO_CONTENT)
-        
-        
 
             
